b2f8a3ebd59284a8bd398ca8ec0f4b17f1d15d06
insecuremanz <86772005+insecuremanz@users.noreply.github.com>
2022-04-05 11:47:18 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -55,7 +55,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/insecuremanz/leviatbox",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
